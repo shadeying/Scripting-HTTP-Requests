f92f52e52ad90c0ee41867afad757bc0afc5d3db
shadeying <shadeying@gmail.com>
2019-05-06 18:25:25 +0000

    Printing in chunks

---
+++ printBody.js
@@ -0,0 +1,24 @@
+var https = require('https');
+
+(function getAndPrintHTMLChunks(){
+
+  var requestOptions = {
+    host: 'sytantris.github.io',
+    path: '/http-examples/step1.html'
+  };
+
+  https.get(requestOptions, function(response){
+
+    response.setEncoding('utf8');
+
+    response.on('data', function(data){
+      console.log('Chunck received: ' + data + '\n');
+    });
+
+    response.on('end', function(){
+      console.log('Response stream completed.');
+    });
+
+  });
+
+})();
